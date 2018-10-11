1a61ac5ad090955e7d5e4683b2b07975a8b8c56d
Student 1870524 <1870524>
2018-10-11 12:42:12 +0200

    DiaThesis Added to Branch

---
+++ DiaThesis/main.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+#include "Scripts/Database/databasecontroller.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    DatabaseController test("http://http://localhost:4181");
+    test.start();
+
+
+    return a.exec();
+}
